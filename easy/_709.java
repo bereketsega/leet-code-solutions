165302e8bd02e94d8b6fbcf5b35bde53fba48256
Bereket Sega <bereketsega8@gmail.com>
2022-02-12 00:55:46 -0500

    solved 709. To Lower Case

---
+++ easy/_709.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class _709 {
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+}
