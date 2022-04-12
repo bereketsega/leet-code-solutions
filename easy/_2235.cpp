1362515fba123e801fe8e6be60413a6af2547b88
Bereket Sega <bereketsega8@gmail.com>
2022-04-12 18:34:23 -0400

    solved 2235. Add Two Integers T/S:O(1)/O(1)

---
+++ easy/_2235.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+
+    int num1 = 12, num2 = 5;
+
+    cout << sum(num1, num2) << endl;
+
+    return -1;
+}
+
+/*
+Given two integers num1 and num2, return the sum of the two integers. 
+*/
