c0719c1d32eb43da8a0c3c1703d9601796ae481c
Bereket Sega <bereketsega8@gmail.com>
2022-08-28 14:42:10 -0400

    solved 434. Number of Segments in a String T/S : O(n)/O(n)

---
+++ easy/_434.py
@@ -0,0 +1,11 @@
+
+def countSegments(s: str) -> int:
+    return len(s.split())
+
+s = "Hello, my name is John"
+print(countSegments(s))
+
+
+# Given a string s, return the number of segments in the string.
+
+# A segment is defined to be a contiguous sequence of non-space characters.
